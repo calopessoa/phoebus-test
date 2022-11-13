277e783df27cdb4547ee4bcb74e96cad170202f7
Carlos <nashlasterby@gmail.com>
2022-11-13 14:52:09 -0300

    feat: first interface test done, cypress.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
 describe('Townsq front-page tests', () => {
   it('should correctly visit and render the main page', () => {
-    cy.visit('https://www.phoebus.com.br/')
+    cy.visit('https://www.phoebus.com.br/');
   });
+
+  it('clicks to redirect to Phoebus` about us', () => {
+    cy.xpath('//*[@id="menu-item-54"]/a/cufon[2]/canvas').click();
+  });
+
+  it('should click on `Nossa História` to redirect to Phoebus` history', () => {
+    cy.xpath('//*[@id="quem-somos"]/div/div/ul/li[1]/a').click();
+  });
+
+  it('should render a `H3 - Revolução do Conceito de POS` on screen', () => {
+    cy.xpath('//*[@id="nossahistoria"]/div/div[2]/div[4]/div[1]/h3');
+  });
+  it('confirms that `Nossos Diferenciais` are available', () => {
+    cy.get('select').select('https://www.phoebus.com.br/quem-somos/diferenciais/')
+  })
 })
